ebc2f164bb88eb21fff17b0772b4685d9a423af9
kewde <kewde@particl.io>
2026-04-28 11:05:55 +0200

    feat(product): add ProductData with parsing and serialization
    - Add load_archive() and serialize_* standalone functions
    - Add ProductData storing raw parsed master, catalog, hardware, applications
    - Add ProductData.from_archive() and to_*_xml() methods
    - Add VersionError, ParseError to errors.py
    - Expose version constants from xknx.models

--- packages/models/src/xknx/models/__init__.py
+++ packages/models/src/xknx/models/__init__.py
@@ -1,4 +1,39 @@
-from xknx.models import generated
+import re
+
+from xknx.models.generated import v10, v11, v12, v13, v14, v20, v21, v22, v23
 
 __version__ = "0.1.0"
-__all__ = ["generated"]
+
+SUPPORTED_VERSIONS = frozenset({"10", "11", "12", "13", "14", "20", "21", "22", "23"})
+
+VERSION_PATTERN = re.compile(rb'xmlns="http://knx\.org/xml/project/(\d+)"')
+
+VERSION_MODULES = {
+    "10": v10,
+    "11": v11,
+    "12": v12,
+    "13": v13,
+    "14": v14,
+    "20": v20,
+    "21": v21,
+    "22": v22,
+    "23": v23,
+}
+
+VERSION_NAMESPACES = {v: f"http://knx.org/xml/project/{v}" for v in SUPPORTED_VERSIONS}
+
+__all__ = [
+    "SUPPORTED_VERSIONS",
+    "VERSION_MODULES",
+    "VERSION_NAMESPACES",
+    "VERSION_PATTERN",
+    "v10",
+    "v11",
+    "v12",
+    "v13",
+    "v14",
+    "v20",
+    "v21",
+    "v22",
+    "v23",
+]
