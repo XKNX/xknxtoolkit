2db23f412411db88629f44d8c95aab4d5364e2e1
kewde <kewde@particl.io>
2026-05-12 19:16:27 +0200

    fix(knx-gui): merge history panel dot and restore button into single column
    Column width calculated from button text size to prevent text shifting

--- apps/knx-gui/src/knx_gui/plugins/project/ui/history.py
+++ apps/knx-gui/src/knx_gui/plugins/project/ui/history.py
@@ -32,6 +32,10 @@ class HistoryPanel:
             imgui.text_disabled(S.HISTORY_NO_HISTORY)
             return
 
+        style = imgui.get_style()
+        btn_text_size = imgui.calc_text_size(S.HISTORY_REVERT)
+        col_width = btn_text_size.x + style.frame_padding.x * 2
+
         for entry in entries:
             is_current = entry.id == cursor
 
@@ -42,32 +46,32 @@ class HistoryPanel:
                 )
 
             row_min_y = imgui.get_cursor_screen_pos().y
+
             if is_current:
                 draw_list = imgui.get_window_draw_list()
                 cursor_pos = imgui.get_cursor_screen_pos()
                 text_height = imgui.get_text_line_height()
-                center = imgui.ImVec2(cursor_pos.x + 6, cursor_pos.y + text_height / 2)
+                center = imgui.ImVec2(cursor_pos.x + col_width / 2, cursor_pos.y + text_height / 2)
                 draw_list.add_circle_filled(
                     center, 4, imgui.get_color_u32(imgui.ImVec4(0.2, 0.8, 0.3, 1.0))
                 )
-            imgui.dummy(imgui.ImVec2(14, 0))
+                imgui.dummy(imgui.ImVec2(col_width, 0))
+            else:
+                mouse_pos = imgui.get_mouse_pos()
+                window_pos = imgui.get_window_pos()
+                row_height = imgui.get_text_line_height_with_spacing()
+                row_hovered = (
+                    row_min_y <= mouse_pos.y < row_min_y + row_height
+                    and window_pos.x <= mouse_pos.x < window_pos.x + col_width + 20
+                )
+                if row_hovered:
+                    if imgui.small_button(f"{S.HISTORY_REVERT}##{entry.id}"):
+                        self._on_jump_to(entry.id)
+                else:
+                    imgui.dummy(imgui.ImVec2(col_width, 0))
+
             imgui.same_line()
-
             imgui.text(entry.display_text)
-            row_max_y = imgui.get_cursor_screen_pos().y
-
-            mouse_pos = imgui.get_mouse_pos()
-            window_pos = imgui.get_window_pos()
-            window_size = imgui.get_window_size()
-            row_hovered = (
-                row_min_y <= mouse_pos.y < row_max_y
-                and window_pos.x <= mouse_pos.x < window_pos.x + window_size.x
-            )
-
-            if row_hovered and not is_current:
-                imgui.same_line(imgui.get_content_region_avail().x - 50)
-                if imgui.small_button(f"{S.HISTORY_REVERT}##{entry.id}"):
-                    self._on_jump_to(entry.id)
 
             if entry.reverted:
                 imgui.pop_style_color()
